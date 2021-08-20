d90115b1ab5502c4db3c0beec117fdae52ec495c
vitorhermeto <54818607+vitorhermeto@users.noreply.github.com>
2021-08-20 02:37:43 -0300

    primeiro commit

---
+++ android/app/src/main/kotlin/com/example/mobx_flutterando/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mobx_flutterando
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("MobX"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '$counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
